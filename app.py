65db3fa17a79bc75b0925b386474262fc0114df7
dkim157 <63015027+dkim157@users.noreply.github.com>
2023-01-22 22:28:55 -0800

    done

--- app.py
+++ app.py
@@ -32,6 +32,45 @@ scenes = {
     '2.09': ['Isabel', 'Camille', 'Shandeigh', 'Jett', 'Roselyn']
 }
 
+cast = {
+    'Camille': ['M', 'T', 'Th', 'F'],
+    'Isabel': ['M', 'Th', 'F'],
+    'Roselyn': ['M', 'T', 'Th', 'F'],
+    'Shandeigh': ['M', 'T'],
+    'Jett': ['M', 'T', 'Th', 'F'],
+    'Bryant': ['M', 'F'],
+    'Zac': ['T', 'Th', 'F'],
+    'Nate': ['M', 'T', 'Th', 'F'],
+    'Megan': ['M', 'T', 'Th', 'F'],
+    'Seth': ['M', 'T', 'Th', 'F'],
+    'Zeya': ['M', 'T', 'Th'],
+    'Rocco': ['M', 'Th', 'F'],
+    'Josue': ['T', 'F'],
+    'Caleb': [],
+    'Figgy': ['M', 'T', 'Th', 'F'],
+    'Isaiah': ['M', 'T', 'Th', 'F'],
+    'Joy': ['M'],
+    'Jessica': ['M', 'T', 'Th', 'F'],
+    'Analisa': ['T', 'Th', 'F'],
+    'Britney': [],
+    'Jackie': ['T', 'Th', 'F'],
+    'Andru': ['T', 'Th', 'F'],
+    'Von': ['M', 'Th', 'F'],
+    'Young Isabel': ['T', 'F'],
+    'Michaela': ['M', 'F'],
+    'Ethan': ['M', 'T', 'Th', 'F'],
+    'Jehlia': ['M', 'T', 'Th', 'F'],
+    'Natalie': ['M', 'F'],
+    'Ally': ['M', 'F'],
+    'Daisy': ['M', 'T', 'Th', 'F'],
+    'Lea Michele': ['M', 'T', 'Th', 'F'],
+    'Christina': ['M', 'T', 'Th'],
+    'Autumn': ['F'],
+    'Meesh': ['M', 'Th', 'F'],
+    'Sharon': [],
+    'Kimiko': ['M', 'F']
+}
+
 def select_day():
     while True:
         day = input('Select a day (M/T/Th/F): ')
@@ -40,15 +79,46 @@ def select_day():
         else:
             print('Select a specified day')
 
-def select_scenes():
-    line = input('Choose 1 or more scenes (separated by space): ')
+def all_scenes_exist(line):
+    invalid_scenes = []
+    # validate each scene in input
     for s in line.split():
-        if s
+        if not (s in scenes):
+            print(s + " is not a valid scene")
+            invalid_scenes.append(s)
+    # return True if all scenes valid, False otherwise
+    if len(invalid_scenes)>0: return False
+    else: return True
 
+def select_scenes():
+    while True:
+        line = input('Choose 1 or more scenes (separated by space): ')
+        if all_scenes_exist(line):
+            return line.split()
 
-def verify():
+def print_availabilities(scene, available, unavailable):
+    print("\nScene " + scene)
+    print("  available: " + ", ".join(available))
+    print("  unavailable: " + ", ".join(unavailable))
+
+def verify_one(scene, day):
+    available = []
+    unavailable = []
+    # populate available and unavailable ppl lists for that day
+    for a in scenes[scene]:
+        if day in cast[a]:
+            available.append(a)
+        else: unavailable.append(a)
+    # prints results
+    print_availabilities(scene, available, unavailable)
+
+def verify_all():
     day = select_day()
-    scenes = select_scenes()
+    selected_scenes = select_scenes()
+    # print availabilities for each scene
+    for s in selected_scenes:
+        verify_one(s, day)
+    print()
 
 if __name__ == '__main__':
-    verify()
+    verify_all()
